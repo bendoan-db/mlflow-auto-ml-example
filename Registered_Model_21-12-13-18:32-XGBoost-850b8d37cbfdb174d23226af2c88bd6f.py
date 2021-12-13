17550b6739bf2e347e18ee78b1d35f9571d649ee
ben.doan@databricks.com <ben.doan@databricks.com>
2021-12-13 21:04:37 +0000

    adding model notebook and prediction examples

---
+++ Registered_Model_21-12-13-18:32-XGBoost-850b8d37cbfdb174d23226af2c88bd6f.py
@@ -0,0 +1,281 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Notebook for Registered Model
+# MAGIC 
+# MAGIC Generated via running automl on the `dec21_flightschool_team2.turbines_silver` table; classifies the `STATUS` column into `healthy` or `damaged` classes
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # XGBoost training
+# MAGIC This is an auto-generated notebook. To reproduce these results, attach this notebook to the **Shared Autoscaling Americas** cluster and rerun it.
+# MAGIC - Compare trials in the [MLflow experiment](#mlflow/experiments/2492745853146109/s?orderByKey=metrics.%60val_f1_score%60&orderByAsc=false)
+# MAGIC - Navigate to the parent notebook [here](#notebook/2492745853146099) (If you launched the AutoML experiment using the Experiments UI, this link isn't very useful.)
+# MAGIC - Clone this notebook into your project folder by selecting **File > Clone** in the notebook toolbar.
+# MAGIC 
+# MAGIC Runtime Version: _9.1.x-cpu-ml-scala2.12_
+
+# COMMAND ----------
+
+import mlflow
+import databricks.automl_runtime
+
+# Use MLflow to track experiments
+mlflow.set_experiment("/Users/ben.doan@databricks.com/databricks_automl/STATUS_turbines_gold-2021_12_13-13_31")
+
+target_col = "STATUS"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Load Data
+# MAGIC > **NOTE:** The dataset loaded below is a sample of the original dataset.
+# MAGIC Stratified sampling using pyspark's [sampleBy](https://spark.apache.org/docs/latest/api/python/reference/api/pyspark.sql.DataFrameStatFunctions.sampleBy.html)
+# MAGIC method is used to ensure that the distribution of the target column is retained.
+# MAGIC <br/>
+# MAGIC > Rows were sampled with a sampling fraction of **0.012809281008372605**
+
+# COMMAND ----------
+
+from mlflow.tracking import MlflowClient
+import os
+import uuid
+import shutil
+import pandas as pd
+
+# Create temp directory to download input data from MLflow
+input_temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], str(uuid.uuid4())[:8])
+os.makedirs(input_temp_dir)
+
+
+# Download the artifact and read it into a pandas DataFrame
+input_client = MlflowClient()
+input_data_path = input_client.download_artifacts("53d1ce0fc29843709ee74b1641c2411e", "data", input_temp_dir)
+
+df_loaded = pd.read_parquet(os.path.join(input_data_path, "training_data"))
+# Delete the temp data
+shutil.rmtree(input_temp_dir)
+
+# Preview data
+df_loaded.head(5)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Preprocessors
+
+# COMMAND ----------
+
+transformers = []
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC %md ### Datetime Preprocessor
+# MAGIC For each datetime column, extract relevant information from the date:
+# MAGIC - Unix timestamp
+# MAGIC - whether the date is a weekend
+# MAGIC - whether the date is a holiday
+# MAGIC 
+# MAGIC Additionally, extract extra information from columns with timestamps:
+# MAGIC - hour of the day (one-hot encoded)
+# MAGIC 
+# MAGIC For cyclic features, plot the values along a unit circle to encode temporal proximity:
+# MAGIC - hour of the day
+# MAGIC - hours since the beginning of the week
+# MAGIC - hours since the beginning of the month
+# MAGIC - hours since the beginning of the year
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+from databricks.automl_runtime.sklearn import TimestampTransformer
+
+for col in ["TIMESTAMP"]:
+    timestamp_transformer = TimestampTransformer()
+    ohe_transformer = ColumnTransformer(
+        [("ohe", OneHotEncoder(sparse=False, handle_unknown="ignore"), [timestamp_transformer.HOUR_COLUMN_INDEX])],
+        remainder="passthrough")
+    timestamp_preprocessor = Pipeline([
+        ("extractor", timestamp_transformer),
+        ("onehot_encoder", ohe_transformer)
+    ])
+    transformers.append((f"timestamp_{col}", timestamp_preprocessor, [col]))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Numerical columns
+# MAGIC 
+# MAGIC Missing values for numerical columns are imputed with mean for consistency
+
+# COMMAND ----------
+
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+
+numerical_pipeline = Pipeline(steps=[
+    ("converter", FunctionTransformer(lambda df: df.apply(pd.to_numeric, errors="coerce"))),
+    ("imputer", SimpleImputer(strategy="mean"))
+])
+
+transformers.append(("numerical", numerical_pipeline, ["AN10", "AN3", "AN4", "AN5", "AN6", "AN7", "AN8", "AN9", "ID", "SPEED"]))
+
+# COMMAND ----------
+
+from sklearn.compose import ColumnTransformer
+
+preprocessor = ColumnTransformer(transformers, remainder="passthrough", sparse_threshold=0)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Feature standardization
+# MAGIC Scale all feature columns to be centered around zero with unit variance.
+
+# COMMAND ----------
+
+from sklearn.preprocessing import StandardScaler
+
+standardizer = StandardScaler()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Training - Validation Split
+# MAGIC Split the input data into training and validation data
+
+# COMMAND ----------
+
+from sklearn.model_selection import train_test_split
+
+split_X = df_loaded.drop([target_col], axis=1)
+split_y = df_loaded[target_col]
+
+X_train, X_val, y_train, y_val = train_test_split(split_X, split_y, random_state=338605691, stratify=split_y)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Train classification model
+# MAGIC - Log relevant metrics to MLflow to track runs
+# MAGIC - All the runs are logged under [this MLflow experiment](#mlflow/experiments/2492745853146109/s?orderByKey=metrics.%60val_f1_score%60&orderByAsc=false)
+# MAGIC - Change the model parameters and re-run the training cell to log a different trial to the MLflow experiment
+# MAGIC - To view the full list of tunable hyperparameters, check the output of the cell below
+
+# COMMAND ----------
+
+from xgboost import XGBClassifier
+
+help(XGBClassifier)
+
+# COMMAND ----------
+
+import mlflow
+import sklearn
+from sklearn import set_config
+from sklearn.pipeline import Pipeline
+
+set_config(display="diagram")
+
+xgbc_classifier = XGBClassifier(
+  learning_rate=0.35201716953832485,
+  max_depth=7,
+  min_child_weight=7,
+  subsample=0.854341238250961,
+  random_state=338605691,
+)
+
+model = Pipeline([
+    ("preprocessor", preprocessor),
+    ("standardizer", standardizer),
+    ("classifier", xgbc_classifier),
+])
+
+model
+
+# COMMAND ----------
+
+# Enable automatic logging of input samples, metrics, parameters, and models
+mlflow.sklearn.autolog(log_input_examples=True, silent=True)
+
+with mlflow.start_run(run_name="xgboost") as mlflow_run:
+    model.fit(X_train, y_train)
+    
+    # Training metrics are logged by MLflow autologging
+    # Log metrics for the validation set
+    xgbc_val_metrics = mlflow.sklearn.eval_and_log_metrics(model, X_val, y_val,
+                                                                prefix="val_")
+    display(pd.DataFrame(xgbc_val_metrics, index=[0]))
+
+# COMMAND ----------
+
+# Patch requisite packages to the model environment YAML for model serving
+import os
+import shutil
+import uuid
+import yaml
+
+None
+None
+
+import holidays
+import xgboost
+from mlflow.tracking import MlflowClient
+
+xgbc_temp_dir = os.path.join(os.environ["SPARK_LOCAL_DIRS"], str(uuid.uuid4())[:8])
+os.makedirs(xgbc_temp_dir)
+xgbc_client = MlflowClient()
+xgbc_model_env_path = xgbc_client.download_artifacts(mlflow_run.info.run_id, "model/conda.yaml", xgbc_temp_dir)
+xgbc_model_env_str = open(xgbc_model_env_path)
+xgbc_parsed_model_env_str = yaml.load(xgbc_model_env_str, Loader=yaml.FullLoader)
+
+xgbc_parsed_model_env_str["dependencies"][-1]["pip"].append(f"holidays=={holidays.__version__}")
+xgbc_parsed_model_env_str["dependencies"][-1]["pip"].append(f"xgboost=={xgboost.__version__}")
+
+with open(xgbc_model_env_path, "w") as f:
+  f.write(yaml.dump(xgbc_parsed_model_env_str))
+xgbc_client.log_artifact(run_id=mlflow_run.info.run_id, local_path=xgbc_model_env_path, artifact_path="model")
+shutil.rmtree(xgbc_temp_dir)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Inference
+# MAGIC [The MLflow Model Registry](https://docs.databricks.com/applications/mlflow/model-registry.html) is a collaborative hub where teams can share ML models, work together from experimentation to online testing and production, integrate with approval and governance workflows, and monitor ML deployments and their performance. The snippets below show how to add the model trained in this notebook to the model registry and to retrieve it later for inference.
+# MAGIC 
+# MAGIC > **NOTE:** The `model_uri` for the model already trained in this notebook can be found in the cell below
+# MAGIC 
+# MAGIC ### Register to Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC 
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC registered_model_version = mlflow.register_model(model_uri, model_name)
+# MAGIC ```
+# MAGIC 
+# MAGIC ### Load from Model Registry
+# MAGIC ```
+# MAGIC model_name = "Example"
+# MAGIC model_version = registered_model_version.version
+# MAGIC 
+# MAGIC model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_version}")
+# MAGIC model.predict(input_X)
+# MAGIC ```
+# MAGIC 
+# MAGIC ### Load model without registering
+# MAGIC ```
+# MAGIC model_uri = f"runs:/{ mlflow_run.info.run_id }/model"
+# MAGIC 
+# MAGIC model = mlflow.pyfunc.load_model(model_uri)
+# MAGIC model.predict(input_X)
+# MAGIC ```
+
+# COMMAND ----------
+
+# model_uri for the generated model
+print(f"runs:/{ mlflow_run.info.run_id }/model")
